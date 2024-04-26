3096dea6217c7192cb1d97b4ebc687df42c2eb53
lvhoushuai <lvhoushuai@sensetime.com>
2024-04-26 15:35:11 +0800

    office365 graphClient 国家云认证

--- src/main/java/org/example/EmailTest_2.java
+++ src/main/java/org/example/EmailTest_2.java
@@ -1,4 +1,15 @@
-//package org.example;
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
 //
 //import java.io.BufferedReader;
 //import java.io.DataOutputStream;
@@ -10,54 +21,57 @@
 //import java.util.HashMap;
 //import java.util.Map;
 //
-//public class EmailTest_2 {
-//
-//    private static final String ROOTURL = "https://microsoftgraph.chinacloudapi.cn/v1.0/";
-//
-//    public static void main(String[] args) throws Exception {
-//        String token = getAuth();
-//
-//    //  String url=  "https://microsoftgraph.chinacloudapi.cn/v1.0/me/messages/AAMkADE0ZmQzMzA4LTQyMjItNDE5Yy05ZDk5LTNhYzk2MGY3YTlkZgBGAAAAAABmYGf9rkEhSqZ7iAqOfm9GBwBv4xUfVx3KTaylTeKC-4XUAAAAAAEMAABv4xUfVx3KTaylTeKC-4XUAAChqd6IAAA=/createReplyAll";
-////        new AuthorizationCodeProvider();
-////        GraphServiceClient graphServiceClient=GraphServiceClient.builder().authenticationProvider();
-//    }
-//
-//    public static String getAuth() throws Exception {
-//        String oauth_url = "https://login.chinacloudapi.cn/" + O365TENANTID + "/oauth2/token";
-//        String resource_url = "https://microsoftgraph.chinacloudapi.cn";
-//
-//        Map<String, String> parameters = new HashMap<>();
-//        parameters.put("grant_type", "client_credentials");
-//        parameters.put("resource", resource_url);
-//        parameters.put("client_id", O365APPID);
-//        parameters.put("client_secret", O365SECRET);
-//
-//        HttpURLConnection connection = (HttpURLConnection) new URL(oauth_url).openConnection();
-//        connection.setRequestMethod("POST");
-//        connection.setDoOutput(true);
-//        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-//        out.writeBytes(getParamsString(parameters));
-//        out.flush();
-//        out.close();
-//
-//        int responseCode = connection.getResponseCode();
-//        if (responseCode == HttpURLConnection.HTTP_OK) {
-//            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            String inputLine;
-//            StringBuffer content = new StringBuffer();
-//            while ((inputLine = in.readLine()) != null) {
-//                content.append(inputLine);
-//            }
-//            in.close();
-//            connection.disconnect();
-//
-//            System.out.println("token:" + content.toString());
-//        } else {
-//            throw new Exception("Failed to get auth: " + responseCode);
-//        }
-//
-//        return  oauth_url;
-//    }
+public class EmailTest_2 {
+    //
+    private static final String ROOTURL = "https://microsoftgraph.chinacloudapi.cn/v1.0/";
+
+    //
+    public static void main(String[] args) throws Exception {
+        String token = getAuth();
+
+        //  String url=  "https://microsoftgraph.chinacloudapi.cn/v1.0/me/messages/AAMkADE0ZmQzMzA4LTQyMjItNDE5Yy05ZDk5LTNhYzk2MGY3YTlkZgBGAAAAAABmYGf9rkEhSqZ7iAqOfm9GBwBv4xUfVx3KTaylTeKC-4XUAAAAAAEMAABv4xUfVx3KTaylTeKC-4XUAAChqd6IAAA=/createReplyAll";
+//        new AuthorizationCodeProvider();
+//        GraphServiceClient graphServiceClient=GraphServiceClient.builder().authenticationProvider();
+    }
+
+    //
+    public static String getAuth() throws Exception {
+        String oauth_url = "https://login.chinacloudapi.cn/a07a5f5d-229551-9c0e-5b98a8a5dffd/oauth2/token";
+        String resource_url = "https://microsoftgraph.chinacloudapi.cn";
+
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("grant_type", "client_credentials");
+        parameters.put("resource", resource_url);
+//        parameters.put("client_id", "31618fcd-333a-4dd8-b7-a894bc559885");
+//        parameters.put("client_secret", "xZ61r-.33VmPu-jHxm571ALbjw8.XE6_");
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(oauth_url).openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+        out.writeBytes(getParamsString(parameters));
+        out.flush();
+        out.close();
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            connection.disconnect();
+
+            System.out.println("token:" + content.toString());
+        } else {
+            throw new Exception("Failed to get auth: " + responseCode);
+        }
+
+        return oauth_url;
+    }
+
 //
 //    public static void getUserAmt(String url, String token) throws Exception {
 //        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
@@ -83,17 +97,17 @@
 //        }
 //    }
 //
-//    public static String getParamsString(Map<String, String> params) throws UnsupportedEncodingException {
-//        StringBuilder result = new StringBuilder();
-//        for (Map.Entry<String, String> entry : params.entrySet()) {
-//            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-//            result.append("=");
-//            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-//            result.append("&");
-//        }
-//        String resultString = result.toString();
-//        return resultString.length() > 0 ? resultString.substring(0, resultString.length() - 1) : resultString;
-//    }
-//}
+    public static String getParamsString(Map<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            result.append("&");
+        }
+        String resultString = result.toString();
+        return resultString.length() > 0 ? resultString.substring(0, resultString.length() - 1) : resultString;
+    }
+}
 //
 //
